54bc6a3ab72456549d3456a2a1dcacb9a8861374
JummyT <167038914+JummyT@users.noreply.github.com>
2025-06-30 16:02:47 +0100

    Added registration flow and console output for confirmation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from student import Student
 # --- Simulate Scenario ---
 if __name__ == "__main__":
     # Create a course
-    python_course = Course(course_id=101, title="Intro to Software Engineering", credits=3)
+    python_course = Course(course_id=101, title="Intro to Software Engineering", credits=5)
 
     # Create a student
     student_olajumoke = Student(student_id=1, name="Olajumoke Toriola", email="S282238@UOS.AC.UK")
