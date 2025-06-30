e3c95b038812ce9ce01bc2a2b2e90bf40c93d03a
JummyT <167038914+JummyT@users.noreply.github.com>
2025-06-30 18:11:59 +0100

    Added changes to details of course_id and Student_id

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from student import Student
 # --- Simulate Scenario ---
 if __name__ == "__main__":
     # Create a course
-    python_course = Course(course_id=101, title="Intro to Software Engineering", credits=5)
+    python_course = Course(course_id=UMDCOM202-92022, title="Intro to Software Engineering", credits=5)
 
     # Create a student
-    student_olajumoke = Student(student_id=1, name="Olajumoke Toriola", email="S282238@UOS.AC.UK")
+    student_olajumoke = Student(student_id=6891110, name="Olajumoke Toriola", email="S28223800@UOS.AC.UK")
 
     # Student registers for a course
     student_olajumoke.register(python_course)
